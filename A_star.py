bd8677e46465e61707ac6cdfd370f3b73e10d3b0
James Gaboardi <jgaboardi@fsu.edu>
2015-08-21 10:11:15 -0400

    update
    update

---
+++ A_star.py
@@ -0,0 +1,23 @@
+'''
+GNU LESSER GENERAL PUBLIC LICENSE
+                       Version 3, 29 June 2007
+ Copyright (C) 2007 Free Software Foundation, Inc. <http://fsf.org/>
+ Everyone is permitted to copy and distribute verbatim copies
+ of this license document, but changing it is not allowed.
+'''
+# The A* algorithm
+#    in Python/Gurobi[gurobipy]
+'''
+Adapted from:
+    Miller, H. J.
+    Shaw, S-L
+    2001
+    Geographic Information Systems for Transportation: 
+        Principles and Applications
+    New York: Oxford University Press
+'''
+
+
+print '*************************************************************************'
+print '-- The A* Algorithm --'
+print '\nJames Gaboardi, 2015'
