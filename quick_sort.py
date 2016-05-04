bb8a248d7743cc55c9e0e24428cd7ee620f367b9
James Gaboardi <jgaboardi@fsu.edu>
2016-05-04 13:31:45 -0400

    commit
    commit

---
+++ quick_sort.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+'''
+GNU LESSER GENERAL PUBLIC LICENSE
+                       Version 3, 29 June 2007
+ Copyright (C) 2007 Free Software Foundation, Inc. <http://fsf.org/>
+ Everyone is permitted to copy and distribute verbatim copies
+ of this license document, but changing it is not allowed.
+'''
+
+import numpy as np
+
+def sub_sort(array,low,high):
+    key = array[low]
+    while low < high:
+        while low < high and array[high] >= key:
+            high -= 1
+        while low < high and array[high] < key:
+            array[low] = array[high]
+            low += 1
+            array[high] = array[low]
+    array[low] = key
+    return low
+
+def quick_sort(array,low,high):
+    if low < high:
+        key_index = sub_sort(array,low,high)
+        quick_sort(array,low,key_index)
+        quick_sort(array,key_index+1,high)
+
+
+array = np.random.randint(2,100,20)
+print '\n Original Array --> ', array
+
+quick_sort(array,0,len(array)-1)
+print '\n Sorted Array   --> ', array
+
+'''
+James Gaboardi, 2016
+'''
